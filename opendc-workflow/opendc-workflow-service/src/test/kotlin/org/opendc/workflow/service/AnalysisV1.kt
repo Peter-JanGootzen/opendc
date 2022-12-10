4ee9c3423fe4a993b57586d6484738138487ed64
11831626 <skip11@live.nl>
2022-12-10 12:55:47 +0100

    Included energyUsage and cpuUtilization over multiple cycles in the monitor.

--- opendc-workflow/opendc-workflow-service/src/test/kotlin/org/opendc/workflow/service/AnalysisV1.kt
+++ opendc-workflow/opendc-workflow-service/src/test/kotlin/org/opendc/workflow/service/AnalysisV1.kt
@@ -36,7 +36,10 @@ import org.opendc.experiments.compute.setupComputeService
 import org.opendc.experiments.compute.telemetry.ComputeMonitor
 import org.opendc.experiments.compute.registerComputeMonitor
 import org.opendc.experiments.compute.setupHosts
+import org.opendc.experiments.compute.telemetry.table.HostInfo
 import org.opendc.experiments.compute.telemetry.table.HostTableReader
+import org.opendc.experiments.compute.telemetry.table.ServerInfo
+import org.opendc.experiments.compute.telemetry.table.ServerTableReader
 import org.opendc.experiments.compute.telemetry.table.ServiceTableReader
 import org.opendc.experiments.compute.topology.HostSpec
 import org.opendc.experiments.provisioner.Provisioner
@@ -55,14 +58,22 @@ import org.opendc.simulator.compute.power.CpuPowerModels
 import org.opendc.simulator.flow2.mux.FlowMultiplexerFactory
 import org.opendc.simulator.kotlin.runSimulation
 import org.opendc.trace.Trace
+import org.opendc.workflow.service.scheduler.job.LimitJobAdmissionPolicy
 import org.opendc.workflow.service.scheduler.job.NullJobAdmissionPolicy
+import org.opendc.workflow.service.scheduler.job.SizeJobOrderPolicy
 import org.opendc.workflow.service.scheduler.job.SubmissionTimeJobOrderPolicy
 import org.opendc.workflow.service.scheduler.task.NullTaskEligibilityPolicy
+import org.opendc.workflow.service.scheduler.task.RandomTaskEligibilityPolicy
+import org.opendc.workflow.service.scheduler.task.RandomTaskOrderPolicy
 import org.opendc.workflow.service.scheduler.task.SubmissionTimeTaskOrderPolicy
 import java.io.File
 import java.nio.file.Paths
 import java.time.Duration
 import java.util.UUID
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
 
 class TestComputeMonitor : ComputeMonitor {
     var attemptsSuccess = 0
@@ -70,6 +81,9 @@ class TestComputeMonitor : ComputeMonitor {
     var attemptsError = 0
     var serversPending = 0
     var serversActive = 0
+    var hostsUp = 0
+    var hostsDown = 0
+    var serversTotal = 0
 
     override fun record(reader: ServiceTableReader) {
         attemptsSuccess = reader.attemptsSuccess
@@ -77,14 +91,25 @@ class TestComputeMonitor : ComputeMonitor {
         attemptsError = reader.attemptsError
         serversPending = reader.serversPending
         serversActive = reader.serversActive
+        hostsUp = reader.hostsUp
+        hostsDown = reader.hostsDown
+        serversTotal = reader.serversTotal
     }
 
     var idleTime = 0L
     var activeTime = 0L
     var stealTime = 0L
     var lostTime = 0L
-    var energyUsage = 0.0
+    //    var energyUsage = 0.0
+//    var powerUsage = 0.0
+    var energyUsage: List<Double> = listOf()
+    var powerUsage: List<Double> = listOf()
     var uptime = 0L
+    var downtime = 0L
+    var cpuLimit = 0.0
+    var cpuUsage = 0.0
+    var cpuDemand = 0.0
+    var cpuUtilization: List<Double> = listOf()
 
     override fun record(reader: HostTableReader) {
         idleTime += reader.cpuIdleTime
@@ -92,7 +117,132 @@ class TestComputeMonitor : ComputeMonitor {
         stealTime += reader.cpuStealTime
         lostTime += reader.cpuLostTime
         energyUsage += reader.powerTotal
+        powerUsage += reader.powerUsage
         uptime += reader.uptime
+        downtime += reader.downtime
+        cpuLimit += reader.cpuLimit
+        cpuUsage += reader.cpuUsage
+        cpuDemand += reader.cpuDemand
+        cpuUtilization += reader.cpuUtilization
+    }
+
+    var serverCpuLimit = 0.0
+    var serverCpuActiveTime = 0L
+    var serverCpuIdleTime = 0L
+    var serverCpuStealTime = 0L
+    var serverCpuLostTime = 0L
+    var sysUptime = 0L
+    var sysDowntime = 0L
+    var serverProvisionTime = ""
+    var sysBootTime = ""
+    var timestamp = ""
+    //    var serverInfo: ServerInfo? = null
+//    var hostInfo: HostInfo? = null
+    var serverInfo: List<ServerInfo?> = listOf()
+    var hostInfo: List<HostInfo?> = listOf()
+
+    override fun record(reader: ServerTableReader) {
+        sysUptime += reader.uptime
+        sysDowntime += reader.downtime
+        serverCpuLimit += reader.cpuLimit
+        serverCpuActiveTime += reader.cpuActiveTime
+        serverCpuIdleTime += reader.cpuIdleTime
+        serverCpuStealTime += reader.cpuStealTime
+        serverCpuLostTime += reader.cpuLostTime
+//        serverProvisionTime = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss").format(LocalDateTime.ofInstant(reader.provisionTime, ZoneOffset.UTC))
+//        sysBootTime = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss").format(LocalDateTime.ofInstant(reader.bootTime, ZoneOffset.UTC))
+//        timestamp = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss").format(LocalDateTime.ofInstant(reader.timestamp, ZoneOffset.UTC))
+//        serverInfo += reader.server
+//        hostInfo += reader.host
+    }
+
+    fun sec2date(secondsLong: Long): String {
+        var seconds = secondsLong.toDouble()
+        var minute = 60.0
+        var hour = 60.0 * minute
+        var day = 24.0 * hour
+        var month = 30.0 * day
+        var year = 12.0 * month
+        var years = seconds / year
+        var months = (seconds % year) / month
+        var days = ((seconds % year) % month) / day
+        var hours = (((seconds % year) % month) % day) / hour
+        var minutes = ((((seconds % year) % month) % day) % hour) / minute
+        var seconds2 = (((((seconds % year) % month) % day) % hour) % minute) / seconds
+        return "${years.toInt()}y${months.toInt()}m${days.toInt()}d ${hours.toInt()}h${minutes.toInt()}m${seconds2.toInt()}s"
+    }
+
+    fun show(title: String, fPath: String="") {
+        var metrics_str: String = "V===================================\n" +
+            "| ${title} - ComputeMonitor\n" +
+            "|-----------------------------------\n" +
+            "| HostTableReader:\n" +
+            "| \tidleTime = ${idleTime} seconds (${sec2date(idleTime)})\n" +
+            "| \tactiveTime = ${activeTime} seconds (${sec2date(activeTime)})\n" +
+            "| \tstealTime = ${stealTime} seconds (${sec2date(stealTime)})\n" +
+            "| \tlostTime = ${lostTime} seconds (${sec2date(lostTime)})\n" +
+            "| \tenergyUsage (J per cycle):\n" +
+            "| \t\tcontent = ${energyUsage}\n" +
+            "| \t\tcycles = ${energyUsage.size}\n" +
+            "| \t\taverage = ${energyUsage.average()} Joules (${energyUsage.average() / 3600} Wh)\n" +
+            "| \t\ttotal = ${energyUsage.sum()} Joules (${energyUsage.sum() / 3600} Wh)\n" +
+            "| \tpowerUsage (W per cycle):\n" +
+            "| \t\tcontent = ${powerUsage}\n" +
+            "| \t\tcycles = ${powerUsage.size}\n" +
+            "| \t\taverage = ${powerUsage.average()} W\n" +
+            "| \t\ttotal = ${powerUsage.sum()} W\n" +
+            "| \tuptime = ${uptime} ms (${sec2date(uptime / 1000)})\n" +
+            "| \tdowntime = ${downtime} ms (${sec2date(downtime / 1000)})\n" +
+            "| \tcpuLimit = ${cpuLimit} MHz\n" +
+            "| \tcpuDemand = ${cpuDemand} MHz\n" +
+            "| \tcpuUtilization (% per cycle):\n" +
+            "| \t\tcontent = ${cpuUtilization}\n" +
+            "| \t\tcycles = ${cpuUtilization.size}\n" +
+            "| \t\taverage = ${cpuUtilization.average()}%\n" +
+            "| \t\ttotal = ${cpuUtilization.sum()}%\n" +
+            "|-----------------------------------\n" +
+            "| ServiceTableReader:\n" +
+            "| \tattemptsSuccess = ${attemptsSuccess}\n" +
+            "| \tattemptsFailure = ${attemptsFailure}\n" +
+            "| \tattemptsError = ${attemptsError}\n" +
+            "| \tserversPending = ${serversPending}\n" +
+            "| \tserversActive = ${serversActive}\n" +
+            "| \thostsUp = ${hostsUp}\n" +
+            "| \thostsDown = ${hostsDown}\n" +
+            "| \tserversTotal = ${serversTotal}\n" +
+            "|-----------------------------------\n" +
+            "| ServerTableReader:\n" +
+            "| \tcpuLimit = ${serverCpuLimit} MHz\n" +
+            "| \tcpuActiveTime = ${serverCpuActiveTime} seconds (${sec2date(serverCpuActiveTime)})\n" +
+            "| \tcpuIdleTime = ${serverCpuIdleTime} seconds (${sec2date(serverCpuIdleTime)})\n" +
+            "| \tcpuStealTime = ${serverCpuStealTime} seconds (${sec2date(serverCpuStealTime)})\n" +
+            "| \tcpuLostTime = ${serverCpuLostTime} seconds (${sec2date(serverCpuLostTime)})\n" +
+            "| \tsysUptime = ${sysUptime} ms (${sec2date(sysUptime / 1000)})\n" +
+            "| \tsysDowntime = ${sysDowntime} ms (${sec2date(sysDowntime / 1000)})\n" +
+//            "| \tserverProvisionTime = ${serverProvisionTime}\n" +
+//            "| \tsysBootTime = ${sysBootTime}\n" +
+//            "| \ttimestamp = ${timestamp}\n" +
+//            "| \tserverInfo:\n" +
+//            "| \t\tid = ${serverInfo?.id}\n" +
+//            "| \t\tname = ${serverInfo?.name}\n" +
+//            "| \t\ttype = ${serverInfo?.type}\n" +
+//            "| \t\tarch = ${serverInfo?.arch}\n" +
+//            "| \t\timageId = ${serverInfo?.imageId}\n" +
+//            "| \t\timageName = ${serverInfo?.imageName}\n" +
+//            "| \t\tcpuCount = ${serverInfo?.cpuCount}\n" +
+//            "| \t\tmemCapacity = ${serverInfo?.memCapacity}\n" +
+//            "| \thostInfo:\n" +
+//            "| \t\tid = ${hostInfo?.id}\n" +
+//            "| \t\tname = ${hostInfo?.name}\n" +
+//            "| \t\tarch = ${hostInfo?.arch}\n" +
+//            "| \t\tcpuCount = ${hostInfo?.cpuCount}\n" +
+//            "| \t\tmemCapacity = ${hostInfo?.memCapacity}\n" +
+//            "| \tserverInfo = ${hostInfo}\n" +
+//            "| \thostInfo = ${hostInfo}\n" +
+            "Ʌ===================================\n"
+
+        println(metrics_str)
+        if (fPath != "") { File(fPath).printWriter().use { out -> out.println(metrics_str) } }
     }
 }
 
@@ -107,6 +257,7 @@ internal class AnalysisV1 {
         val workflowService = "workflow.opendc.org"
         val monitor = TestComputeMonitor()
 
+//        Random.nextLong()
         Provisioner(coroutineContext, clock, seed = 0L).use { provisioner ->
             val scheduler: (ProvisioningContext) -> ComputeScheduler = {
                 FilterScheduler(
@@ -127,9 +278,12 @@ internal class AnalysisV1 {
                     WorkflowSchedulerSpec(
                         schedulingQuantum = Duration.ofMillis(100),
                         jobAdmissionPolicy = NullJobAdmissionPolicy,
+//                        jobAdmissionPolicy = LimitJobAdmissionPolicy(0),
                         jobOrderPolicy = SubmissionTimeJobOrderPolicy(),
+//                        jobOrderPolicy = SizeJobOrderPolicy(),
                         taskEligibilityPolicy = NullTaskEligibilityPolicy,
-                        taskOrderPolicy = SubmissionTimeTaskOrderPolicy()
+//                        taskEligibilityPolicy = RandomTaskEligibilityPolicy(),
+                        taskOrderPolicy = RandomTaskOrderPolicy
                     )
                 ),
                 registerComputeMonitor(computeService, monitor)
@@ -144,18 +298,8 @@ internal class AnalysisV1 {
             service.replay(clock, trace.toJobs())
         }
 
-
-        println(
-            "Scheduler " +
-                "Success=${monitor.attemptsSuccess} " +
-                "Failure=${monitor.attemptsFailure} " +
-                "Error=${monitor.attemptsError} " +
-                "Pending=${monitor.serversPending} " +
-                "Active=${monitor.serversActive} " +
-                "Energy=${monitor.energyUsage} " +
-                "Uptime=${monitor.uptime}"
-        )
-        writeMonitorStats(monitor)
+        monitor.show("testTrace", "results.txt")
+//        writeMonitorStats(monitor)
     }
 
     private fun writeMonitorStats(monitor : TestComputeMonitor) {
@@ -185,7 +329,7 @@ internal class AnalysisV1 {
             "host-$uid",
             emptyMap(),
             machineModel,
-            SimPsuFactories.simple(CpuPowerModels.linear(100.0, 50.0)), /* TODO: REALISTIC POWER VALUES */
+            SimPsuFactories.simple(CpuPowerModels.linear(350.0, 200.0)), /* TODO: REALISTIC POWER VALUES */
             FlowMultiplexerFactory.forwardingMultiplexer()
         )
     }
